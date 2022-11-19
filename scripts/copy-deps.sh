6d257d8441f43dd557abc6b0b1b16f7d7488360b
Jason Dagit <dagitj@gmail.com>
2022-11-19 01:03:52 -0800

    Refactor copy-deps, maybe this will fix it

--- scripts/copy-deps.sh
+++ scripts/copy-deps.sh
@@ -2,16 +2,49 @@
 HERE="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 echo HERE=$HERE
 
-set -ex
+set -e
+
+# This isn't actually meant to be run on linux, the linux commands are for
+# testing and developing the script on linux. So they just echo the commands.
+
+function install_name {
+  if [ $(uname) == "Linux" ]
+  then
+    echo install_name_tool $*
+  elif [ $(uname) == "Darwin" ]
+  then
+    install_name_tool $*
+  fi
+}
+
+function get_deps {
+  if [ $(uname) == "Linux" ]
+  then
+    ldd "$1" | grep "=>" | awk '{ print $3 }' | xargs
+  elif [ $(uname) == "Darwin" ]
+  then
+    otool -L "$1" | grep "/*.*/dylib" -o | grep -v '/usr/lib' | grep -v '/System/Library' | xargs
+  fi
+}
+
+function copy_file {
+  if [ $(uname) == "Linux" ]
+  then
+    echo cp $*
+  elif [ $(uname) == "Darwin" ]
+  then
+    cp $*
+  fi
+}
 
 function copy_dep {
   local source="$1"
   local target="$2/$(dirname $source)"
   mkdir -p "$target"
-  cp "$source" "$target"
-  install_name_tool -change "$source" "@executable_path/../Resources/libs/$source" "$3"
+  copy_file "$source" "$target"
+  install_name -change "$source" "@executable_path/../Resources/libs/$source" "$3"
 
-  deps=$(otool -L "$source" | grep "/*.*dylib" -o | grep -v '/usr/lib' | grep -v '/System/Library')
+  deps=$(get_deps "$source")
   for d in $deps
   do
     if ! [[ -f "$2/$d" ]]
@@ -19,12 +52,11 @@ function copy_dep {
       copy_dep "$d" "$2" "$2/$d"
     fi
   done
-
 }
 
 binary=$HERE/../target/release/bundle/osx/Annelid.app/Contents/MacOS/annelid
 dest=$HERE/../target/release/bundle/osx/Annelid.app/Contents/Resources/libs
-deps=$(otool -L $binary | grep "/*.*dylib" -o | grep -v '/usr/lib' | grep -v '/System/Library' | xargs)
+deps=$(get_deps "$binary")
 
 for d in $deps
 do
