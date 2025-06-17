0e45197ca9f4965676fc27202b6a6f8b6200cb08
Jason Dagit <dagitj@gmail.com>
2025-06-16 18:12:57 -0700

    json autosplitter fixes from parsing real examples

--- src/autosplitters/json.rs
+++ src/autosplitters/json.rs
@@ -44,6 +44,7 @@ pub struct CheckDetails {
 
 #[derive(Debug, Clone, Deserialize, Serialize)]
 pub struct Splits {
+    #[serde(alias = "name", alias = "game")]
     pub game: String,
     pub autostart: Option<Autostart>,
     pub definitions: Vec<Check>,
@@ -63,12 +64,51 @@ impl Splits {
     }
 }
 
+#[derive(Debug, Clone, Serialize)]
+pub enum Autostart {
+    Inactive { note: Option<String> },
+    Active { check: CheckDetails },
+}
+
+impl<'de> Deserialize<'de> for Autostart {
+    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let raw = RawAutostart::deserialize(deserializer)?;
+
+        if raw.active {
+            let address = raw
+                .address
+                .ok_or_else(|| de::Error::missing_field("address"))?;
+            let value = raw.value.ok_or_else(|| de::Error::missing_field("value"))?;
+            let typ = raw.typ.ok_or_else(|| de::Error::missing_field("type"))?;
+
+            let address = from_hex_str::<D>(&address)?;
+            let value = from_hex_str::<D>(&value)?;
+            Ok(Autostart::Active {
+                check: CheckDetails {
+                    note: raw.note,
+                    address,
+                    value,
+                    typ,
+                },
+            })
+        } else {
+            Ok(Autostart::Inactive { note: raw.note })
+        }
+    }
+}
+
 #[derive(Debug, Clone, Deserialize, Serialize)]
-pub struct Autostart {
+pub struct RawAutostart {
     #[serde(deserialize_with = "string_to_bool")]
     pub active: bool,
-    #[serde(flatten)]
-    pub check: CheckDetails,
+    pub note: Option<String>,
+    pub address: Option<String>,
+    pub value: Option<String>,
+    #[serde(rename = "type")]
+    pub typ: Option<CheckType>,
 }
 
 fn from_hex<'de, D>(deserializer: D) -> Result<u32, D::Error>
@@ -76,8 +116,19 @@ where
     D: Deserializer<'de>,
 {
     let s: &str = Deserialize::deserialize(deserializer)?;
-    u32::from_str_radix(s.trim().trim_start_matches("0x"), 16)
-        .map_err(|e| de::Error::custom(format!("invalid hex: {}", e)))
+    from_hex_str::<D>(s)
+}
+
+fn from_hex_str<'de, D>(s: &str) -> Result<u32, D::Error>
+where
+    D: Deserializer<'de>,
+{
+    let s = s.trim();
+    let s = s
+        .strip_prefix("0x")
+        .or_else(|| s.strip_prefix("0X"))
+        .unwrap_or(s);
+    u32::from_str_radix(s, 16).map_err(|e| de::Error::custom(format!("invalid hex: {}", e)))
 }
 
 fn string_to_bool<'de, D>(deserializer: D) -> Result<bool, D::Error>
