99f485120c0e1f817c59a46925436c588f77ed8d
Douglas Kirby <douglas.r.kirby@gmail.com>
2025-07-01 22:22:50 -0400

    location update; generic game selector attempt

--- src/autosplitters.rs
+++ src/autosplitters.rs
@@ -1,8 +1,9 @@
 pub mod json;
-pub mod battletoads;
+pub mod nwa;
 pub mod supermetroid;
 use anyhow::Result;
-use std::net::Ipv4Addr;
+// use std::net::Ipv4Addr;
+// use std::error::Error;
 use livesplit_core::{GameTime, TimeSpan};
 
 #[derive(Debug, Copy, Clone, serde::Deserialize, serde::Serialize, PartialEq)]
@@ -13,16 +14,23 @@ pub enum AType {
     CUSTOM,
 }
 
-// Not sure how to do this...
-pub fn AutoSplitterSelector(game: &str,reset_timer_on_game_reset: bool) -> Object {
-    match game {
-        "Battletoads" => return battletoads::battletoadsAutoSplitter::new(
-                            Ipv4Addr::new(0, 0, 0, 0),
-                            48879, reset_timer_on_game_reset
-                        ),
-        &_ => todo!()
-    }
+#[derive(Debug, PartialEq, Clone, Copy)]
+pub enum Game {
+    Battletoads,
+    SuperMetroid,
+    None,
 }
+
+// // Not sure how to do this...
+// pub fn AutoSplitterSelector(game: &str,reset_timer_on_game_reset: bool) -> Result<Game, Box<dyn Error>> {
+//     match game {
+//         "Battletoads" => Ok(Game::Battletoads(battletoadsAutoSplitter::new(
+//                             Ipv4Addr::new(0, 0, 0, 0),
+//                             48879, reset_timer_on_game_reset
+//                         ))),
+//         _ => panic!("Worker type not found")
+//     }
+// }
 
 #[derive(Debug, Copy, Clone)]
 pub struct NWASummary {
